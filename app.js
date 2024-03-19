30d78bbbcc01f37ba1b82d0ce9188f45b832e292
PIETRA PAULA VOLPATO <23111825@CFP127ALUNO.LOCAL>
2024-03-19 16:50:43 -0300

    traducao-finalizada

---
+++ app.js
@@ -0,0 +1,46 @@
+const textoEntra = document.querySelector("#TextoEntra");
+const textoSai = document.querySelector("#TextoSai");
+const Traduzir = document.querySelector("#Traduzir");
+const selects = document.querySelectorAll("select");
+
+const linguas = {
+  "en-GB": "Inglês",
+  "pt-BR": "Português",
+  "it-IT": "Italiano",
+  "ja-JP": "Japonês",
+  "es-ES": "Espanhol"
+};
+
+selects.forEach((tag) => {
+  for (paises in linguas) {
+  let selected;
+   if (tag.className.includes("selectFrom") && paises == "pt-BR") {
+     selected = "selected";
+   } else if (tag.className.includes("selectTo") && paises == "en-GB") {
+  selected = "selected";
+ }
+
+   const option = `<option value="${paises}" ${selected}>${linguas[paises]}</option>`;
+
+  tag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+Traduzir.addEventListener("click", () => {
+  if (textoEntra.value) {
+    TraduzirLoad();
+  } else {
+    textoSai.value = "";
+  }
+});
+
+function TraduzirLoad() {
+  fetch(
+    `https://api.mymemory.translated.net/get?q=${textoEntra.value}&langpair=pt-BR|en-GB`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(textoEntra)
+      textoSai.value = data.responseData.translatedText;
+    });
+}
